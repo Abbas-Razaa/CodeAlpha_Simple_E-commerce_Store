4deee2c5161a17ba2da63618411480a349f11210
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-31 22:48:45 +0500

    add to cart & checkout completed

--- index.js
+++ index.js
@@ -49,16 +49,22 @@ app.get('/', (req, res) => {
     });
 
     con.query("SELECT * FROM products", (err,result)=>{
-        res.render('pages/index', {result:result});
+        if (err) {
+            console.error('Error fetching data: ', err);
+            res.render('pages/index', { result: [] }); // Handle error, send an empty array
+          } else {
+            res.render('pages/index', { result: result });
+          }
     })
 });
 
-app.post('/add_to_cart', function(){
+app.post('/add_to_cart', function(req,res){
     var id = req.body.id;
     var name = req.body.name;
     var price = req.body.price;
     var sale_price = req.body.sale_price;
     var quantity = req.body.image;
+    var image = req.body.image;
     var product = {id: id, name: name, price: price, sale_price: sale_price, quantity: quantity, image: image}
 
     if(req.session.cart){
@@ -79,13 +85,120 @@ app.post('/add_to_cart', function(){
 
 });
 
-app.get('/cart', function(req,res){
+app.get('/cart', function(req, res) {
+    if (!req.session.cart) {
+        req.session.cart = []; // Initialize as an empty array if not already set
+    }
+
+    var cart = req.session.cart;
+    var total = req.session.total || 0;
+
+    res.render('pages/cart', { cart: cart, total: total });
+});
+
+app.post('/remove_product', function(req,res){
+    var id = req.body.id;
+    var cart = req.session.cart;
+    for(let i=0; i<cart.length; i++){
+        if(cart[i].id == id){
+            cart.splice(cart.indexOf(i),1);
+        }
+    }
+
+    calulateTotal(cart,req);
+    res.redirect('/cart');
+});
+
+app.post('/edit_product_quantity', function(req,res){
+
+    var id = req.body.id;
+    var quantity = req.body.quantity;
+    var increase_btn = req.body.increase_product_quantity;
+    var decrease_btn = req.body.decrease_product_quantity;
+    var cart = req.session.cart;
+
+    if(increase_btn){
+        for(let i=0; i<cart.length; i++){
+            if(cart[i].id == id){
+                if(cart[i].quantity > 0){
+
+                    cart[i].quantity = parseInt(cart[i].quantity) + 1;
+                }
+            }
+        }
+    }
+
+    if(decrease_btn){
+        for(let i=0; i<cart.length; i++){
+            if(cart[i].id == id){
+                if(cart[i].quantity > 1){
+                    cart[i].quantity = parseInt(cart[i].quantity) - 1;
+                }
+            }
+        }
+    }
+
+
+    calulateTotal(cart,req);
+
+    res.redirect('/cart');
+
+});
+
+
+app.get('/checkout', function(req, res) {
+
+    var total = req.session.total;
+    res.render('pages/checkout', {total: total});
+});
+
+app.post('/place_order', function(req,res){
     var cart = req.session.cart;
     var total = req.session.total;
+    var  phone = req.body.phone;
+    var city = req.body.city;
+    var address = req.body.address;
+    var cost = req.session.total;
+    var status = 'not paid';
+    var date = new Date();
+    var product_ids = '';
 
-    res.render('pages/cart', {cart: cart, total: total});
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "node_project"
+    });
+
+    var cart = req.session.cart;
+
+    for(let i=0; i<cart.length; i++){
+        product_ids = product_ids + ',' + cart[i].id; 
+    }
+
+    con.connect((err)=>{
+        if(err){
+            console.log(err)
+        } else {
+            var query = "INSERT INTO ORDERS(cost,name,email,status,city,address,phone,date,product_ids) VALUES ?"
+
+            var values = [[cost,name,email,status,city,address,phone,date,product_ids]];
+
+            con.query(query,[values],(err,result)=>{
+                res.redirect('/payment');
+            })
+        }
+        
+    })
     
 });
+
+app.get('/payment', function(req, res) {
+    var total = req.session.total;
+    res.render('pages/payment',{total: total});
+});
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
